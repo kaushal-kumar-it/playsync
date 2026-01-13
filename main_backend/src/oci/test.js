e3f8df4ed3a9e3ba0e6dcc6fe09585da267a7c35
kaushal-kumar-it <techs308@gmail.com>
2026-01-13 18:14:58 +0530

    deleted test file

--- main_backend/src/oci/test.js
+++
@@ -1,6 +0,0 @@
-import * as common from "oci-common";
-
-console.log("Available exports:", Object.keys(common));
-console.log("\nAuth related:", Object.keys(common).filter(k => k.toLowerCase().includes('auth')));
-console.log("\nInstance related:", Object.keys(common).filter(k => k.toLowerCase().includes('instance')));
-console.log("\nConfig related:", Object.keys(common).filter(k => k.toLowerCase().includes('config')));
